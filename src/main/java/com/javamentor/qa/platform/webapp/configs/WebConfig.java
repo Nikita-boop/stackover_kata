11e27ab687cd68af1ef25e2506944a8c30348718
Андрей Яковлев <andrushhaa@mail.ru>
2022-10-05 08:12:01 +0000

    Resolve "Разработать страницу main"

--- src/main/java/com/javamentor/qa/platform/webapp/configs/WebConfig.java
+++ src/main/java/com/javamentor/qa/platform/webapp/configs/WebConfig.java
@@ -10,6 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/questions").setViewName("questions");
+        registry.addViewController("/profile").setViewName("profile");
+        registry.addViewController("/main").setViewName("mainpage");
     }
 }
